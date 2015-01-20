ece325d3b9845a6bfe354cc14c81016da878f05d
Dominique Hostettler <dominiquehostettler@gmail.com>
2015-01-20 08:48:11 +0100

    started work on bullet

--- Games/Runaway/Scripts/gamestage.cs
+++ Games/Runaway/Scripts/gamestage.cs
@@ -18,6 +18,8 @@ namespace Games.Runaway
         protected override void OnEnter()
         {
             Debug.Log("GameStage entered");
+            Graphics.SetClearColor(Game.Swatches.ClearColor);
+            StartCoroutine(SpawnBullets);
         }
         // called each tick
         protected override void OnUpdate()
@@ -29,5 +31,20 @@ namespace Games.Runaway
         {
             base.OnRender();
         }
+
+        IEnumerator SpawnBullets()
+        {
+            while(true)
+            {
+                // wait
+                yield return Delay(0.5f);
+                // spawn bullet
+                float PADDING = 10.0f;
+                Vector2f bulletPos = new Vector2f(Mathf.Random(PADDING, Graphics.Width - PADDING), Graphics.Height + PADDING);
+                Bullet bullet = Add(new Bullet(), 1);
+                bullet.Position = bulletPos;
+            }
+        }
+
     }
 }
